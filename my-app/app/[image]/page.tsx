b1b9e0a53c0b0689d40dcdf8fe6fdaa37b34ec7d
신준혁 <oridori2705@naver.com>
2024-03-22 22:53:45 +0900

    bug: Add img tag

--- my-app/app/[image]/page.tsx
+++ my-app/app/[image]/page.tsx
@@ -4,7 +4,13 @@ export default function ImageTestPage({params}:{params : {image:string}}) {
   const {image} = params;
   console.log("params : "+image)
   const imageSrc= "/worngdata"
+  const imageSrc2= "/worngdata2"
   return (
-   <div><Image src={imageSrc} alt="" fill/></div>
+   <div>
+    <Image src={imageSrc} alt="" fill/>
+    <img src={imageSrc2} alt="" />
+   </div>
   );
 }
+
+// Connect to localhost:3000/1, refresh, and check the terminal
